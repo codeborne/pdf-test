fd6014d4adde635e99716f95db449154de3cf547
Andrei Solntsev <andrei.solntsev@gmail.com>
2015-06-25 20:57:11 +0300

    Add matcher containsExactText

--- src/main/java/com/codeborne/pdftest/PDFMatchers.java
+++ src/main/java/com/codeborne/pdftest/PDFMatchers.java
@@ -1,10 +1,14 @@
 package com.codeborne.pdftest;
 
+import com.codeborne.pdftest.matchers.ContainsExactText;
 import com.codeborne.pdftest.matchers.ContainsText;
 import org.hamcrest.Matcher;
 
 public class PDFMatchers {
   public static Matcher<PDF> containsText(String text) {
     return new ContainsText(text);
+  }
+  public static Matcher<PDF> containsExactText(String text) {
+    return new ContainsExactText(text);
   }
 }
---
+++ src/test/java/com/codeborne/pdftest/matchers/ContainsExactTextTest.java
@@ -0,0 +1,33 @@
+package com.codeborne.pdftest.matchers;
+
+import com.codeborne.pdftest.PDF;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static com.codeborne.pdftest.PDFMatchers.containsExactText;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+
+public class ContainsExactTextTest {
+  @Test
+  public void canAssertThatPdfContainsText() throws IOException {
+    PDF pdf = new PDF(getClass().getClassLoader().getResource("50quickideas.pdf"));
+    assertThat(pdf, containsExactText("50 Quick Ideas to Improve your User Stories"));
+  }
+
+  @Test
+  public void shouldBeCaseSensitive() throws IOException {
+    PDF pdf = new PDF(getClass().getClassLoader().getResource("50quickideas.pdf"));
+    assertThat(pdf, not(containsExactText("50 quick ideas")));
+  }
+
+  @Test
+  public void shouldNotIgnoreWhitespaces() throws IOException {
+    PDF pdf = new PDF(getClass().getClassLoader().getResource("50quickideas.pdf"));
+    assertThat(pdf, containsExactText("Gojko Adzic"));
+    assertThat(pdf, not(containsExactText("Gojko       Adzic")));
+    assertThat(pdf, not(containsExactText("\tGojko \t Adzic\n")));
+    assertThat(pdf, not(containsExactText("Gojko \u00a0 Adzic\r\n")));
+  }
+}
