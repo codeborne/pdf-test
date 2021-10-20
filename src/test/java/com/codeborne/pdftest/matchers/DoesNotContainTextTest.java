e5ee7efc081499ee3f1e555f012ceb817d823667
Vitali Plagov <vplagov@pm.me>
2021-10-20 23:26:42 +0300

    Add test for Hamcrest matchers

--- src/test/java/com/codeborne/pdftest/matchers/DoesNotContainTextTest.java
+++ src/test/java/com/codeborne/pdftest/matchers/DoesNotContainTextTest.java
@@ -1,9 +1,11 @@
 package com.codeborne.pdftest.matchers;
 
 import com.codeborne.pdftest.PDF;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.Objects;
 
 import static com.codeborne.pdftest.PDF.doesNotContainText;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -11,17 +13,25 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.fail;
 
 public class DoesNotContainTextTest {
-  @Test
-  public void canAssertThatPdfContainsText() throws IOException {
-    PDF pdf = new PDF(getClass().getClassLoader().getResource("50quickideas.pdf"));
-    assertThat(pdf, doesNotContainText("42 Quick Ideas"));
-    assertThat(pdf, doesNotContainText("Bruce Willis"));
+
+  private PDF fiftyIdeasPdf;
+
+  @Before
+  public void setUp() throws Exception {
+    fiftyIdeasPdf = new PDF(
+            Objects.requireNonNull(getClass().getClassLoader().getResource("50quickideas.pdf"))
+    );
   }
 
   @Test
-  public void shouldBeCaseSensitive() throws IOException {
-    PDF pdf = new PDF(getClass().getClassLoader().getResource("50quickideas.pdf"));
-    assertThat(pdf, doesNotContainText("50 quick ideas"));
+  public void canAssertThatPdfContainsText() {
+    assertThat(fiftyIdeasPdf, doesNotContainText("42 Quick Ideas"));
+    assertThat(fiftyIdeasPdf, doesNotContainText("Bruce Willis"));
+  }
+
+  @Test
+  public void shouldBeCaseSensitive() {
+    assertThat(fiftyIdeasPdf, doesNotContainText("50 quick ideas"));
   }
 
   @Test
@@ -33,7 +43,17 @@ public class DoesNotContainTextTest {
     }
     catch (AssertionError expected) {
       assertThat(expected.getMessage(),
-          is("\nExpected: a PDF not containing \"Hello World\"\n     but: was \"Hello World\""));
+          is("\nExpected: a PDF not containing <[Hello World]>\n     but: was \"Hello World\""));
     }
+  }
+
+  @Test
+  public void shouldNotContainMultipleTexts() {
+    assertThat(fiftyIdeasPdf, doesNotContainText("42", "Slow", "Applications"));
+  }
+
+  @Test(expected = AssertionError.class)
+  public void shouldFailWhenNotAllTextsAreMissing() {
+    assertThat(fiftyIdeasPdf, doesNotContainText("42", "Slow", "Ideas"));
   }
 }
